26fd01db6f03b9729968575ae8245db861dcca34
asprentaluser1001-hub <asprentaluser1001@gmail.com>
2026-09-26 16:20:00 +0530

    V3 preview: mark preview build

--- client/src/version.ts
+++ client/src/version.ts
@@ -1,4 +1,4 @@
 export const APP_NAME = 'FlingRoulette';
-export const APP_VERSION = '2.0';
-export const BUILD_NUMBER = '2026.09.26.2';
+export const APP_VERSION = '3.0-preview';
+export const BUILD_NUMBER = '2026.09.26.3-preview';
 export const COPYRIGHT_OWNER = 'Sahil Mhatre';
